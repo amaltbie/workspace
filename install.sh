fb7f59ce744443de6f5306e7b324ca041aedfcd1
Arlo Maltbie <arlomltb@gmail.com>
2018-04-21 15:16:47 -0700

    Fix conditional

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 WORKSPACE_DIR="${HOME}/.workspace"
 
-test -d ${WORKSPACE_DIR} && (
-  echo "${WORKSPACE_DIR} already exists";
+if [[ -d ${WORKSPACE_DIR} ]] ; then
+  echo "${WORKSPACE_DIR} already exists"
   exit 1
-)
+fi
 git clone git@github.com:amaltbie/workspace.git ${WORKSPACE_DIR}
